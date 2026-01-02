5b79db661f00c2800c0a9f80c7fce8705a74859c
tipcue <2787773487@qq.com>
2026-01-02 23:25:18 +0800

    去除野指针和%ms这种非标准扩展

--- Assignment-03/assignment3-extra.c
+++ Assignment-03/assignment3-extra.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define MAX 100
 int main(void)
 {
   //initialize vars
-  char *first;
-  char *last;
+  char *first=(char *)malloc(MAX*sizeof(char));
+  char *last=(char *)malloc(MAX*sizeof(char));
 
   //prompt user to input first and last name and use scanf() to store those to the initiliazed vars
   printf("Enter your first name: ");
-  scanf("%ms", &first);
+  scanf("%99s", first);
   printf("Enter your last name: ");
-  scanf("%ms", &last);
+  scanf("%99s", last);
 
   //print the welcome message!
   printf("Hello %s %s!\n", first, last);
