56bf6ab45b01077779b565c54f15631f180c352d
tipcue <2787773487@qq.com>
2026-01-03 22:50:55 +0800

    learn %s need '\0' as a sign of stop.try puts(arry) but seems also need 0 end.without it would lead confusing output

---
+++ Assignment-18/assignment18.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+void hello(void)
+{
+	char array[] = {72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33,'\0'};
+
+	puts(array);
+}
+
+int main(void) 
+{
+	hello();
+}
