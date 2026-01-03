cfa14fd7fa82d4488015eeaf75caffb831705ce2
tipcue <2787773487@qq.com>
2026-01-03 10:29:52 +0800

    changed 10 to 12,leran and practise how to use gcc 编译

--- Assignment-10/assignment10.c
+++ Assignment-10/assignment10.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int array[10] = {0,1,2,3,4,5,6,7,8,9};
+    int array[12] = {0,1,2,3,4,5,6,7,8,9,10,11};
 
     printf("The array has %d elements.\n",sizeof(array) / sizeof(array[0]));
 
