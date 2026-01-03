1633767dec7fd1d4f8f725340481752f1c1a6970
tipcue <2787773487@qq.com>
2026-01-03 14:46:21 +0800

    a simple and easy version,which is nerd.

---
+++ Assignment-11/assignment11_extra.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(){
+    double s[8]={0.0},n;
+    int i;
+    int count=0;
+    for(i=0;i<8;i++){
+        printf("input your score :");
+        scanf("%lf",&n);
+        s[i]=n;
+        count++;
+        printf("wheather continue input? (y/n) :");
+        char c;
+        getchar();
+        scanf("%c",&c);     
+        if(c!='y'){
+            break;
+        }
+    }
+    double sum=0;
+    for(i=0;i<count;i++){
+        sum+=s[i];
+    }
+    if(count>0){
+        double average=sum/count;
+        printf("Average score is: %.2lf\n",average);
+    }else{
+        printf("No scores entered.\n");
+    }
+    return 0;
+}
